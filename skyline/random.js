90cabb6bf07da8d2091a2cdf5d4eb9999b1a2831
CAviv <caviv@users.noreply.github.com>
2023-09-30 22:36:19 +0300

    Added random.js for seeding

---
+++ skyline/random.js
@@ -0,0 +1,41 @@
+// generate seed for randomness
+function cyrb128(str) {
+    let h1 = 1779033703, h2 = 3144134277,
+        h3 = 1013904242, h4 = 2773480762;
+    for (let i = 0, k; i < str.length; i++) {
+        k = str.charCodeAt(i);
+        h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
+        h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
+        h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
+        h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
+    }
+    h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
+    h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
+    h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
+    h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
+    h1 ^= (h2 ^ h3 ^ h4), h2 ^= h1, h3 ^= h1, h4 ^= h1;
+    return [h1>>>0, h2>>>0, h3>>>0, h4>>>0];
+}
+
+// generate random() function from seed
+function xoshiro128ss(a, b, c, d) {
+    return function() {
+        var t = b << 9, r = b * 5; r = (r << 7 | r >>> 25) * 9;
+        c ^= a; d ^= b;
+        b ^= c; a ^= d; c ^= t;
+        d = d << 11 | d >>> 21;
+        return (r >>> 0) / 4294967296;
+    }
+}
+
+// generate random
+let seed = cyrb128(Math.random() + '');
+let random = xoshiro128ss(seed[0], seed[2], seed[2], seed[3]);
+
+function getRandomInt(max) {
+	return Math.floor(random() * Math.floor(max));
+}
+
+function Rand(min, max) {
+	return Math.floor(random() * (max - min + 1)) + min;
+}
